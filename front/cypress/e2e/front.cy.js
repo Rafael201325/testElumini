23cb2f42c3b66f3b342d1c6e3f6718ae842c1554
Rafael Siqueira <rafaelbs94@gmail.com>
2023-04-20 00:47:17 -0300

    front pronto

--- front/cypress/e2e/front.cy.js
+++ front/cypress/e2e/front.cy.js
@@ -1,20 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Test Elumini', () => {
-  it('Go to "Onde usar meu cartão VR"', () => {
-    // 
-      cy.visit('https://www.vr.com.br')
-      //Endereço do site cliente
-      cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click()
-      //Aceitar termos de uso
-      cy.get(':nth-child(3) > .vr-quick-navigation__link').click()
-      //Botão "Para você"
-      cy.get('.vr-hero__actions > .vr-button--negative').click()
-      //Botão "Onde usar meu cartão"
-      cy.get('#map').should('be.visible')
-      //Verificar se o map foi exibido 
-  });
-  // it('Acessar seção "Onde usar meu cartão VR"', () => {});
-
+    it('Acessar "Onde usar meu cartão VR"', () => {
+      // 
+        cy.visit('https://www.vr.com.br')
+        //test address
+        cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click()
+        //Accept terms of use
+        cy.get(':nth-child(3) > .vr-quick-navigation__link').click()
+        //Button "Para você"
+        cy.get('.vr-hero__actions > .vr-button--negative').click()
+        //Button "Onde usar meu cartão"
+        cy.get('#map').should('be.visible')
+        //Check if the map is displayed 
+    });
+    // it('Acessar seção "Onde usar meu cartão VR"', () => {});
   
-  });
+    
+    })
